56e97e0e13a1210d856c6348968652e61f8efedc
ckl <397380372@qq.com>
2026-08-02 10:21:14 +0800

    fix: 解决release版本检测新版本失效问题

--- src/feature_win/resource_win.hpp
+++ src/feature_win/resource_win.hpp
@@ -27,57 +27,59 @@ protected:
         // *.head_img 的请求使用本地保存的头像文件进行响应
         // 文件名为 logo 表示请求读取程序本身的图标
         // 文件名为 数字 表示请求对应资源ID的资源文件
-        this->OnWebRequest([](cxxui::WebRequest& ctx) {
-            auto url = ctx.GetUrl();
-            std::string_view urlv = url;
-            urlv.remove_prefix((std::min)(9, static_cast<int>(urlv.size())));  // http://./
-            auto pos = urlv.find('.');
-            auto name = std::string{urlv.substr(0, pos)};
+        this->OnWebRequest(
+            [](cxxui::WebRequest& ctx) {
+                auto url = ctx.GetUrl();
+                std::string_view urlv = url;
+                urlv.remove_prefix((std::min)(9, static_cast<int>(urlv.size())));  // http://./
+                auto pos = urlv.find('.');
+                auto name = std::string{urlv.substr(0, pos)};
 
-            if (pos != std::string_view::npos) {
-                // 根据扩展名设置 Content-Type
-                urlv.remove_prefix(pos + 1);
-                std::string_view ext = urlv.substr(0, urlv.find_first_of("/?#"));
-                // 加载微信头像
-                if (ext == "head_img") {
-                    try {
-                        ctx.SetHeaders(std::string{ctx.GetContentType("jpg")});
-                        ctx.SetResponse(
-                            (Config::GetInstance().GetUserDir() / name / weixin::kHeadImgName)
-                                .u8string());
-                        return;
-                    } catch (...) {
-                        name = "logo";  // 退化为加载默认logo
+                if (pos != std::string_view::npos) {
+                    // 根据扩展名设置 Content-Type
+                    urlv.remove_prefix(pos + 1);
+                    std::string_view ext = urlv.substr(0, urlv.find_first_of("/?#"));
+                    // 加载微信头像
+                    if (ext == "head_img") {
+                        try {
+                            ctx.SetHeaders(std::string{ctx.GetContentType("jpg")});
+                            ctx.SetResponse(
+                                (Config::GetInstance().GetUserDir() / name / weixin::kHeadImgName)
+                                    .u8string());
+                            return;
+                        } catch (...) {
+                            name = "logo";  // 退化为加载默认logo
+                        }
+                    } else {
+                        ctx.SetHeaders(std::string{ctx.GetContentType(ext)});
                     }
-                } else {
-                    ctx.SetHeaders(std::string{ctx.GetContentType(ext)});
                 }
-            }
 
-            UINT rc_id = 0;
-            if (name == "logo") {
-                rc_id = IDD_LOGO;
-            } else {
-                std::from_chars(name.data(), name.data() + name.size(), rc_id);
-            }
-            HINSTANCE hinst = GetModuleHandle(nullptr);
-            HRSRC hr = FindResource(hinst, MAKEINTRESOURCE(rc_id), RT_RCDATA);
-            if (!hr) {
-                ctx.SetResponse(404);
-                return;
-            }
-            DWORD size = SizeofResource(hinst, hr);
-            if (size <= 0) {
-                ctx.SetResponse(404);
-                return;
-            }
-            HGLOBAL hg = LoadResource(hinst, hr);
-            if (!hg) {
-                ctx.SetResponse(404);
-                return;
-            }
-            ctx.SetResponse(LockResource(hg), size);
-        });
+                UINT rc_id = 0;
+                if (name == "logo") {
+                    rc_id = IDD_LOGO;
+                } else {
+                    std::from_chars(name.data(), name.data() + name.size(), rc_id);
+                }
+                HINSTANCE hinst = GetModuleHandle(nullptr);
+                HRSRC hr = FindResource(hinst, MAKEINTRESOURCE(rc_id), RT_RCDATA);
+                if (!hr) {
+                    ctx.SetResponse(404);
+                    return;
+                }
+                DWORD size = SizeofResource(hinst, hr);
+                if (size <= 0) {
+                    ctx.SetResponse(404);
+                    return;
+                }
+                HGLOBAL hg = LoadResource(hinst, hr);
+                if (!hg) {
+                    ctx.SetResponse(404);
+                    return;
+                }
+                ctx.SetResponse(LockResource(hg), size);
+            },
+            "http://./*");
 #else
         // *.head_img 的请求使用本地保存的头像文件进行响应
         OnWebRequest(
